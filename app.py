d306a43531d412eb72f4c49c358af1fc82338e0d
jochanmin <happyjarban@gmail.com>
2022-05-13 04:51:32 +0000

    feat: load 모듈 함수화

--- app.py
+++ app.py
@@ -8,8 +8,11 @@ from PIL import Image
 import pytesseract 
 import cv2
 import config
+from load import ImageProcessing
 from starlette.responses import Response
 import boto3
+from datetime import datetime
+
 
 app = FastAPI()
 
@@ -27,14 +30,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# TODO: 
-# OCR(??)
-# Hand detection
-# Object detection
-# frame cutting 
+load=ImageProcessing()
 
 client_s3 = boto3.client( 
         's3', 
+        region_name = config.AWS_S3_CONFIG['AWS_REGION'],
         aws_access_key_id = config.AWS_S3_CONFIG['AWS_ACCESS_KEY_ID'], 
         aws_secret_access_key = config.AWS_S3_CONFIG['AWS_SECRET_ACCESS_KEY']
 )
@@ -48,20 +48,20 @@ async def read_root():
 #OCR 인식 Request
 @app.post("/ocr/")
 async def upload_image(
-    image: UploadFile = File(...),
+    imagez: UploadFile = File(...),
     id:  int = Form(...),
     name: str = Form(...),
     ):
     try:
-        image = Image.open(image.file)
+        image = Image.open(imagez.file)
         img = np.array(image)
-        result = scanId(img,id,name)
+        result = load.scanId(img,id,name)
         if result == -1:
             return JSONResponse(status_code=404)
-        else: 
-            res=client_s3.upload_file( config.AWS_S3_CONFIG['AWS_REGION'], config.AWS_S3_CONFIG['AWS_S3_BUCKET_NAME'], result, ExtraArgs={'ContentType': 'image/jpeg'} )
-            print(res)
-            return { url: 'aa' }
+        else:
+            #res=client_s3.upload_file(imagez.file , config.AWS_S3_CONFIG['AWS_S3_BUCKET_NAME'], ExtraArgs={'ACL': 'public-read'} )
+            #print(res)
+            return {'url':'http://naver.com' } 
     except Exception as e:
-        print('POST /image error: %e' % e)
-        return JSONResponse(status_code=500)
+        print('POST /image error: %e' % type(e))
+        return JSONResponse(status_code=500,detail=e)
